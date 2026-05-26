8432e20cbcda90013d9e7ace0851599d56b71ddb
rodrigojunior <rodrigojunior@ljr.eti.br>
2026-05-26 19:02:10 -0300

    Atividade 4.4

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.ts
@@ -8,11 +8,22 @@ import { Component } from '@angular/core';
 })
 export class ListarPensamento {
 
-  listaPensamentos =  [
+   listaPensamentos = [
+/*     {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo3'
+    },
     {
-      conteudo: 'Pensamento 1',
-      autoria: 'Autoria 1',
+      conteudo: 'Minha propriedade é decorada com @Input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim. ',
+      autoria: '',
       modelo: 'modelo1'
-    }
-  ]
+    },
+ */  ];
+
 }
